72ff2a47eebb376fa0e56a39159f42cf02b32195
t01400018 <matheus.rosa@ilia.digital>
2022-04-17 11:50:25 -0300

    feat: install and config cypress
    BREAKING CHANGE: install cypress to e2e tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,4 +12,10 @@ module.exports = {
   env: {
     "vue/setup-compiler-macros": true,
   },
+  overrides: [
+    {
+      files: ["cypress/integration/**.spec.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
+    },
+  ],
 };
--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
 <style>
-@import "@/assets/base.css";
+@import "@/assets/_base.scss";
+@import "@/assets/_normalize.scss";
 
 #app {
   max-width: 1280px;
