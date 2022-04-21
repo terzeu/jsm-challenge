e56d5179e641893ed009e91d25c8339bbd5da2d9
t01400018 <matheus.rosa@ilia.digital>
2022-04-21 12:31:53 -0300

    feat: environment variable to api base url

--- src/config/http.ts
+++ src/config/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'https://jsm-challenge-bff.herokuapp.com',
+  baseURL: import.meta.env.VITE_BASE_API,
   timeout: 15000,
   headers: {
     'cache-control': 'no-cache',
